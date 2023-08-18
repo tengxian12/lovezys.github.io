0feb96ff2fbe66043b61810fd44719f50a970b72
tengxian12 <1926214078@qq.com>
2023-08-19 00:29:01 +0800

    lovezys

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+    if(userName=="张艺上" &&  pwd=="0821"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+		}
+	else{
+		alert("密码错误！请不要乱填呦，嘿嘿！提示：密码是四位数字的生日");
+	}
+});
